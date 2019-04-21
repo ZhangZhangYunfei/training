359f803cf45aa7c6b500c196f13c20902530fe92
Yunfei Zhang <yunfeizhang@YunfeideMacBook-Pro-3.local>
2019-04-22 00:50:34 +0800

    test

--- src/quicksort/QuickSort.java
+++ src/quicksort/QuickSort.java
@@ -1,7 +1,6 @@
 package quicksort;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class QuickSort {
     private static void sort(int[] values, int left, int right) {
@@ -80,11 +79,12 @@ public class QuickSort {
     }
 
     private static int findK(int[] values, int left, int right, int k) {
-        if (left >= right && values.length > 1) {
-            return -1;
+        if (values.length == 1) {
+            return values[0];
         }
-        if (left > right && values.length == 1) {
-            return -1;
+
+        if (left >= right) {
+            return values[left];
         }
 
         int leftTemp = findMiddle(values, left, right);
@@ -98,6 +98,56 @@ public class QuickSort {
                 ? findK(values, left, leftTemp, k)
                 : findK(values, leftTemp + 1, right, k);
     }
+
+    public static int longestConsecutive(int[] nums) {
+        int result = 0;
+
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                continue;
+            }
+            set.remove(num);
+
+            int pre = num - 1;
+            while (set.contains(pre)) {
+                set.remove(pre);
+                pre--;
+            }
+            int next = num + 1;
+            while (set.contains(next)) {
+                set.remove(next);
+                next++;
+            }
+            result = Math.max(result, next - pre - 1);
+        }
+        return result;
+    }
+
+    public String getPermutation(int n, int k) {
+        return "";
+
+    }
+
+    private static void test(int n, List<Integer> resultTemp, List<List<Integer>> result) {
+        for (int index = 1; index <= n; index++) {
+            if (resultTemp.contains(index)) {
+                continue;
+            }
+            resultTemp.add(index);
+            if (resultTemp.size() == n) {
+                result.add(new ArrayList<>(resultTemp));
+                resultTemp.clear();
+                continue;
+            }
+            test(n, resultTemp, result);
+        }
+    }
+
 
 //  private static int findK(int[][] values,
 //                           int[][] leftAndRight,
@@ -146,21 +196,43 @@ public class QuickSort {
 //    return -1;
 //  }
 
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        sort(nums, 0, nums.length - 1);
+        for (int k = 0; k < nums.length; ++k) {
+            if (nums[k] > 0) break;
+            int target = 0 - nums[k];
+            int i = k + 1, j = nums.length - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] == target) {
+                    result.add(Arrays.asList(nums[k], nums[i], nums[j]));
+                    while (i < j && nums[i] == nums[i + 1]) ++i;
+                    while (i < j && nums[j] == nums[j - 1]) --j;
+                    ++i;
+                    --j;
+                } else if (nums[i] + nums[j] < target) ++i;
+                else --j;
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
         for (int i = 0; i < nums.length - 2; i++) {
             List<Integer> arr = new ArrayList<>();
             treeSum(nums, 1, 0, i, arr, result);
         }
 
-        return result;
+        return new ArrayList<>(result);
     }
 
-    private static void treeSum(int[] nums, int path, int value, int index, List<Integer> oneResult, List<List<Integer>> results) {
+    private static void treeSum(int[] nums, int path, int value, int index, List<Integer> oneResult, Set<List<Integer>> results) {
         if (path == 3) {
             for (int i = index; i < nums.length; i++) {
                 if (nums[i] + value == 0) {
                     oneResult.add(nums[i]);
+                    oneResult.sort(Integer::compareTo);
                     results.add(oneResult);
                     break;
                 }
@@ -168,7 +240,7 @@ public class QuickSort {
         } else {
             oneResult.add(nums[index]);
             value = nums[index] + value;
-            for (int j = index + 1; j < nums.length - 1; j++) {
+            for (int j = index + 1; j < nums.length - (2 - path); j++) {
                 treeSum(nums, path + 1, value, j, new ArrayList<>(oneResult), results);
             }
         }
@@ -182,8 +254,8 @@ public class QuickSort {
 //        for (int value : values)
 //            System.out.println(value);
 //
-        int[] values1 = new int[]{99, 99};
-        int result = findK(values1, 0, values1.length - 1, values1.length - 1 + 1);
+//        int[] values1 = new int[]{99, 99};
+//        int result = findK(values1, 0, values1.length - 1, values1.length - 1 + 1);
 //        System.out.println(result);
 //
 //        int[] values2 = new int[]{5, 4, 4, 6, 7, 8, 9, 1, 2, 3, 10, 1};
@@ -191,7 +263,10 @@ public class QuickSort {
 //
 //        int[] values3 = new int[]{5, 4, 4, 6, 7, 8, 9, 1, 2, 3, 10, 1};
 //        int middle3 = findMiddle3(values3, 0, values3.length - 1);
-//        var result = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+//        List<List<Integer>> result = threeSum(new int[]{14, 4, 6, -1, 10, 9, -8, 7, -13, 14, -13, -11, -8, -9, 11, 14, -8, -14, -13, 7, -10, -15, -13, -11, -11, 11, 14, 13, 2, -14, 1, -7, -2, 14, -1, -15, 9, 7, -1, 3, 6, 1, 7, 5, -1, -5, 4, -2, -4, -1, -9, -7, -1, -7, -11, 3, 12, 10, -7, -1, 12, 1, 8, -13, 1, 14, 9, -13, 6, -7, -3, -11, 2, -11, 10, -14, -1, -9, 0, 2, 5, 6, 3, -11, 6, 7, 0, 3, 3, 0, -12, -8, -13, 3, -14, -5, 2, 10, -11, -14, -12, 1, -10, 5, 5, 7, -1, 11, 14, 6, -10, -4, -3, 8, -7, 10, 1, 8, -1, -11, -15, -6, -12, -13, 12, -11});
 
+//        int result = longestConsecutive(new int[]{100, 4, 200, 1, 3, 2});
+
+        test(3, new ArrayList<>(), new ArrayList<>());
     }
 }
